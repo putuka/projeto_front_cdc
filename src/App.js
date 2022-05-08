a8eddd775b3499c36c2222052174e884eef60593
Arthur Vinicius <arthurviniciuseng@gmail.com>
2022-05-08 17:37:29 -0300

    colocando api
    inserindo api no projeto

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Routes, Route} from "react-router-dom";
 import Main from './pages/Main';
 import Conta from './pages/Conta';
 import Login from './pages/Login';
 
-  function App(){
-    return (
+  class App extends Component{ 
+    render(){ 
+      return( 
+
       <div className='App'>
-        
+        <h1></h1>
         <Routes>
           <Route path="/"  element ={<Main/>} />
           <Route path="/conta" element ={<Conta/>} />
           <Route path="/login" element ={<Login/>} />
         </Routes>
       </div>
-    );
-  };
-
+      );};
+    
+}
 export default App;
