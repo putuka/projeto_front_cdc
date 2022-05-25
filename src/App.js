7a748679e1a2d5c2b0da064de0246f4386ba0e13
pedroara <araujo.peo@gmail.com>
2022-05-24 21:45:38 -0300

    Display percent at chart

--- src/App.js
+++ src/App.js
@@ -1,48 +1,51 @@
-import React, { Component } from 'react';
-import {Routes, Route} from "react-router-dom";
-import Conta from './pages/Conta/Conta.js';
-import Login from './pages/Login/Login';
-import Cadastro from './pages/Cadastro/Cadastro';
-import Itens from './pages/Itens/Itens';
-import Main from './pages/Main';
-import Relatorio from './pages/Relatorio/Relatorio';
-import "./App.css"
+import React, { Component } from "react";
+import { Routes, Route } from "react-router-dom";
+import Conta from "./pages/Conta/Conta.js";
+import Login from "./pages/Login/Login";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Itens from "./pages/Itens/Itens";
+import Main from "./pages/Main";
+import Relatorio from "./pages/Relatorio/Relatorio";
+import "./App.css";
 
-
-  class App extends Component{ 
-    render(){ 
-      return(   
-      <div className='App'>
-        
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
         {/* <nav className='Cabecalho'>
           <button type="button" >Conectar</button> 
           <button type="button">Cadastrar</button>
           <button type="button">Minhas Listas</button>
           <button type="button">Relatorio</button>
         </nav> */}
-        <div className='Cabecalho'>
-            <a className='nav-bar-link' href='/login'>Login</a>
-            <a className='nav-bar-link' href='/cadastro'>Cadastro</a>
-            <a className='nav-bar-link' href='/conta'>Minhas Listas</a>
-            <a className='nav-bar-link' href='/relatorio'>Relatório</a> 
+        <div className="Cabecalho">
+          <a className="nav-bar-link" href="/login">
+            Login
+          </a>
+          <a className="nav-bar-link" href="/cadastro">
+            Cadastro
+          </a>
+          <a className="nav-bar-link" href="/conta">
+            Minhas Listas
+          </a>
+          <a className="nav-bar-link" href="/relatorio">
+            Relatório
+          </a>
         </div>
-        
+
         <div>
-        <Routes>
-          <Route path="/"  element ={<Login/>} />
-          <Route path="/conta" element ={<Conta/>} />
-          <Route path="/login" element ={<Login/>} />
-          <Route path="/cadastro" element ={<Cadastro/>} />
-          <Route path="/itens" element ={<Itens/>} />
-          <Route path="/relatorio" element={<Relatorio/>}/>
-          <Route path="*" element ={<Main/>} />
-        </Routes> 
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/conta" element={<Conta />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/itens" element={<Itens />} />
+            <Route path="/relatorio" element={<Relatorio />} />
+            <Route path="*" element={<Main />} />
+          </Routes>
         </div>
       </div>
-      );};   
+    );
+  }
 }
 export default App;
-
-
-
-
--- src/pages/Relatorio/Relatorio.js
+++ src/pages/Relatorio/Relatorio.js
@@ -1,16 +1,15 @@
-import React, { useState, useEffect, Fragment } from 'react'
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import axios from "axios"
+import React, { useState, useEffect, Fragment } from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import axios from "axios";
+import ChartDataLabels from "chartjs-plugin-datalabels";
 
-
-import { Doughnut } from 'react-chartjs-2';
+import { Doughnut } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-
 const api = axios.create({
-    baseURL:'https://carrinhodecontas.herokuapp.com/api/v1/'
-})
+  baseURL: "https://carrinhodecontas.herokuapp.com/api/v1/",
+});
 
 const Relatorio = () => {
   const [dados, setDados] = useState(null);
@@ -30,64 +29,73 @@ const Relatorio = () => {
     if (!dados) {
       getUserdata();
     }
-    console.log("dados:",dados);
-  }, [dados])
+    console.log("dados:", dados);
+  }, [dados]);
 
-  
   var data = {
-    
-    labels: dados?.spentThisYearByCategory?.map(x=>x.name),
-    datasets: [{
-      label: '# of Votes',
-      data: dados?.spentThisYearByCategory?.map(x=>x.spentThisYear),
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }]
+    labels: dados?.spentThisYearByCategory?.map((x) => x.name),
+    datasets: [
+      {
+        label: "",
+        data: dados?.spentThisYearByCategory?.map((x) => x.spentThisYear),
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
   };
 
   var options = {
     maintainAspectRatio: false,
-    scales: {
-    },
+    scales: {},
     legend: {
       labels: {
         fontSize: 25,
       },
     },
-  }
+    plugins: {
+      datalabels: {
+        formatter: (value, ctx) => {
+          let sum = 0;
+          let dataArr = ctx.chart.data.datasets[0].data;
+          dataArr.map((data) => {
+            sum += data;
+          });
+          let percentage = ((value * 100) / sum).toFixed(2) + "%";
+          return percentage;
+        },
+        color: "#fff",
+      },
+    },
+  };
 
   return (
     <>
-    <div>
-      
-      <Doughnut
-        data={data}
-        height={400}
-        options={options}
-      />
-    </div>
-    <div>
-      {"total gasto no ano: "+dados?.spentThisYear}
-    </div>
+      <div>
+        <Doughnut
+          data={data}
+          height={400}
+          options={options}
+          plugins={[ChartDataLabels]}
+        />
+      </div>
+      <div>{"total gasto no ano: " + dados?.spentThisYear}</div>
     </>
-    
-    
-  )
-}
+  );
+};
 
-export default Relatorio
+export default Relatorio;
